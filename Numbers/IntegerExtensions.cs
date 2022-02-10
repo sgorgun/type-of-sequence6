887a4bf3132283a0ecb993e665791196482cbc4d
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-10 11:42:09 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ Numbers/IntegerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Numbers
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Obtains formalized information in the form of an enum <see cref="ComparisonSigns"/>
+        /// about the relationship of the order of two adjacent digits for all digits of a given number.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>Information in the form of an enum <see cref="ComparisonSigns"/>
+        /// about the relationship of the order of two adjacent digits for all digits of a given number
+        /// or null if the information is not defined.</returns>
+        public static ComparisonSigns? GetTypeComparisonSigns(this long number)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets information in the form of a string about the type of sequence that the digit of a given number represents.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>The information in the form of a string about the type of sequence that the digit of a given number represents.</returns>
+        public static string GetTypeOfDigitsSequence(this long number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
