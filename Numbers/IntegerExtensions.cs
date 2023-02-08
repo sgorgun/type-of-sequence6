45a029607566a86f12d2e498f59991547eec5528
@gorgun.sergei <gorgun.sergei@student.ehu.lt>
2023-02-08 02:07:26 +0200

    Ready.  Very short code!

--- Numbers/IntegerExtensions.cs
+++ Numbers/IntegerExtensions.cs
@@ -14,7 +14,39 @@ namespace Numbers
         /// or null if the information is not defined.</returns>
         public static ComparisonSigns? GetTypeComparisonSigns(this long number)
         {
-            throw new NotImplementedException();
+            if (number == long.MinValue)
+            {
+                return ComparisonSigns.MoreThan | ComparisonSigns.Equals | ComparisonSigns.LessThan;
+            }
+
+            if (number < 10)
+            {
+                return null;
+            }
+
+            ComparisonSigns? comparisonSigns = 0;
+
+            while (number >= 10)
+            {
+                long termA = number % 10;
+                number /= 10;
+                long termB = number % 10;
+
+                if (termA > termB)
+                {
+                    comparisonSigns |= ComparisonSigns.LessThan;
+                }
+                else if (termA < termB)
+                {
+                    comparisonSigns |= ComparisonSigns.MoreThan;
+                }
+                else
+                {
+                    comparisonSigns |= ComparisonSigns.Equals;
+                }
+            }
+
+            return comparisonSigns;
         }
 
         /// <summary>
@@ -24,7 +56,18 @@ namespace Numbers
         /// <returns>The information in the form of a string about the type of sequence that the digit of a given number represents.</returns>
         public static string GetTypeOfDigitsSequence(this long number)
         {
-            throw new NotImplementedException();
+            ComparisonSigns? comparisonSigns = number.GetTypeComparisonSigns();
+
+            return comparisonSigns switch
+            {
+                null => "One digit number.",
+                ComparisonSigns.LessThan | ComparisonSigns.Equals => "Increasing.",
+                ComparisonSigns.MoreThan | ComparisonSigns.Equals => "Decreasing.",
+                ComparisonSigns.LessThan => "Strictly Increasing.",
+                ComparisonSigns.MoreThan => "Strictly Decreasing.",
+                ComparisonSigns.Equals => "Monotonous.",
+                _ => "Unordered."
+            };
         }
     }
 }
